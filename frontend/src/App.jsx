4f3530aa239f4c3e3e09ec69e4849466886563db
Diana Amillano <estephanirobles23@gmail.com>
2025-09-03 02:05:32 -0600

    login react terminado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ export const App = () => {
         <Route path="/" element={<HomePage/>} />
         <Route path="/register" element={<RegisterPage/>} />
         <Route path="/login" element={<LoginPage/>} />
+
+        
         <Route path="/profile" element={<ProfilePage/>} />
       </Routes>
     </BrowserRouter>
