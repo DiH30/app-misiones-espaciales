af943990f9fca925e52a41fe16af767ab37fa26a
Diana Amillano <estephanirobles23@gmail.com>
2025-09-22 06:36:00 -0600

    cors origin and methods

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,8 @@ const app = express()
 
 app.use( express.json() )
 app.use( cors({
-  origin: "http://localhost:5173",
+  origin: "https://app-misiones-espaciales-izgi.vercel.app",
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }) )
 app.use( cookieParser() )
