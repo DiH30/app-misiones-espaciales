18e863cfdfa0224d1ea04554c0fb6ba3dfa6e2b0
Diana Amillano <estephanirobles23@gmail.com>
2025-09-24 00:08:15 -0600

    act archivos del backend: package.json, vercel.json y app.js.

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -2,31 +2,38 @@ import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import { config } from "dotenv"
-import misionesRouter from "./routes/misiones.routes.js"
-import authRoutes from "./routes/auth.routes.js"
+import misionesRouter from "../routes/misiones.routes.js"
+import authRoutes from "../routes/auth.routes.js"
 import cookieParser from "cookie-parser"
 
-config()
+config();
 
 const PORT = process.env.PORT
-const app = express()
+const app = express();
 
-app.use( express.json() )
-app.use( cors({
+app.use(express.json());
+app.use( 
+  cors({
   origin: [
     "http://localhost:5173",
+    // Agregar aquí el dominio del frontend en producción
     "https://app-misiones-espaciales-izgi.vercel.app"
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+  credentials: true,
 }) )
-app.use( cookieParser() )
+app.use(cookieParser());
 
+// Rutas
 app.use("/api/misiones", misionesRouter)
 app.use( "/api", authRoutes )
 
+// Conexión a MongoDB 
 mongoose.connect(process.env.MONGO_KEY).then( () => console.log( "Conectado a MongoDB" ) )
 
 app.listen(PORT, () => {
   console.log("Servidor corriendo en el puerto", PORT)
 })
+
+// Exportar app para que Vercel la use como función serverless
+export default app;
