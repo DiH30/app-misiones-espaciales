e7f05c18b4505da7b495e9e62d8de8748435de7c
Diana Amillano <estephanirobles23@gmail.com>
2025-09-24 00:57:37 -0600

    implementación de proxy

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,11 +4,11 @@ import { authRequired } from "../middlewares/validateToken.js";
 import { loginValidator, registerValidator } from "../validators/auth.validator.js";
 import { handleValidator } from "../middlewares/handleValidator.js";
 
-const router = Router()
+const router = Router();
 
 router.post('/register', registerValidator, handleValidator, register)
 router.post('/login', loginValidator, handleValidator, login)
 router.post('/logout', logout)
 router.get('/profile', authRequired, profile)
 
-export default router
+export default router;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    proxy: {
+      // Redirigir cualquier solicitud a /api al backend en Vercel
+      '/api': 'https://app-misiones-espaciales.vercel.app',
+    },
+  },
 })
